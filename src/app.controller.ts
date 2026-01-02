95c6d6f48cd412b4776dc397c3920d4d547aa148
CyberArt2718281 <artem2006pax@mail.ru>
2026-01-02 03:22:20 +0300

    added guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { StringToLowercasePipe } from './shared/pipes/stringToLowercase.pipe';
+import { AuthGuard } from './shared/guards/auth.guard'
 
 @Controller('app')
 export class AppController {
@@ -7,5 +8,15 @@ export class AppController {
   @Post()
   create(@Body('title') title: string) {
     return `Movie ${title}`;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(){
+    return {
+      id: 1,
+      email: 'artem1234@mail.ru',
+      userName: 'artyom_dev',
+    }
   }
 }
