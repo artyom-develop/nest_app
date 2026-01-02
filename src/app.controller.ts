92cc5ed2d483cfe5a47c7c22bc76845cdeaca843
CyberArt2718281 <artem2006pax@mail.ru>
2026-01-02 03:34:22 +0300

    added custom decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { StringToLowercasePipe } from './shared/pipes/stringToLowercase.pipe';
-import { AuthGuard } from './shared/guards/auth.guard'
+import { AuthGuard } from './shared/guards/auth.guard';
+import { UserAgent } from './shared/decotators/user-agent.decorator';
 
 @Controller('app')
 export class AppController {
@@ -12,11 +20,12 @@ export class AppController {
 
   @UseGuards(AuthGuard)
   @Get('profile')
-  getProfile(){
+  getProfile(@UserAgent() userAgent: string) {
     return {
       id: 1,
       email: 'artem1234@mail.ru',
       userName: 'artyom_dev',
-    }
+      userAgent: userAgent,
+    };
   }
 }
